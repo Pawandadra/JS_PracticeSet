f0e89992c361e6052ac8e98a3572b84e884044c7
Pawan <pawankumarpk3610@gmail.com>
2024-06-14 13:34:13 +0530

    added functionality for BOM

--- Random_num_game/script.js
+++ Random_num_game/script.js
@@ -1,6 +1,3 @@
-const prompt = require("prompt-sync")()
-console.log("Welcome to The World of - Guess and Input. \n")
-
 const getRandomNum = (max) => {
    return Math.floor(Math.random() * max)
 }
@@ -17,15 +14,16 @@ do {
    tries++
    
    if (inpNum > random_num){
-   console.log("Your guess is greater than random number. Please try again !!")
+   alert("Your guess is greater than random number. Please try again !!")
    }  
    else if (inpNum < random_num){
-      console.log("Your guess is smaller than random number. Please try again !!")
+      alert("Your guess is smaller than random number. Please try again !!")
    } 
    else if (inpNum == random_num){
-      console.log("Congratulations !! You guessed the correct Number.\n Your Score is : "+ (101-tries))
+      alert("Congratulations !! You guessed the correct Number.\n Your Score is : "+ (101-tries))
+      break
    } 
    else {
-      console.log("You entered invalid data. Please try again !!")
+      alert("You entered invalid data. Please try again !!")
    }
 } while (inpNum !== random_num);
