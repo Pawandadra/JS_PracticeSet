cb7318444dd4c1bda01ec0bc6f5cbf08b886180c
Pawan <pawankumarpk3610@gmail.com>
2024-06-14 13:13:10 +0530

    optimized

--- Snake_Water_Gun_Game/script.js
+++ Snake_Water_Gun_Game/script.js
@@ -2,7 +2,7 @@ const random_no = (max) => {
     return Math.floor(Math.random() * max)
   }
   let cpu
-  const getCpuChoice =(cpu) =>{
+  const getCpuChoice =() =>{
     cpu = random_no(3)
     switch(cpu){ 
       case 0:
@@ -20,27 +20,19 @@ const random_no = (max) => {
   do{
     cpu = getCpuChoice(cpu)
     user = prompt("Enter S, W or G").toUpperCase()
-    if (user == "S" && cpu == "W"){
+
+    if (user == "S" && cpu == "W" || user == "W" && cpu == "G" || user == "G" && cpu == "S"){
       alert("Congratulations!! You win.")
     }
-    else if (user == "S" && cpu == "G"){
+
+    else if (user == "S" && cpu == "G" || user == "W" && cpu == "S" || user == "G" && cpu == "W" ){
       alert("You lose!!")
     }
-    else if (user == "W" && cpu == "S"){
-      alert("You lose!!")
-    }
-    else if (user == "W" && cpu == "G"){
-      alert("Congratulations!! You win.")
-    }
-    else if (user == "G" && cpu == "S"){
-      alert("Congratulations!! You win.")
-    }
-    else if (user == "G" && cpu == "W"){
-      alert("You lose!!")
-    }
+   
     else if (user == "S" && cpu == 0 || user == "W" && cpu == 1 || user == "G" && cpu == 2){
       alert("Both have same choices.")
     }
+    
     else{
       alert("Invalid choice.")
     }
